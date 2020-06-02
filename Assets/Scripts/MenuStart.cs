dc777573c514c8b8bd3e275b04ed5d67c1d0480f
AnnPla <anna.platek2222@gmail.com>
2020-06-02 17:14:51 +0200

    add coin pickup prefab

--- Assets/Scripts/MenuStart.cs
+++ Assets/Scripts/MenuStart.cs
@@ -5,16 +5,35 @@ using UnityEngine.SceneManagement;
 
 public class MenuStart : MonoBehaviour
 {
+    //GameObject gameSession;
+
     public void StartFirstLEvel()
     {
+        ResetGameSession();
         SceneManager.LoadScene(1);
     }
     public void BackToMenu()
     {
+        ResetGameSession();
         SceneManager.LoadScene(0);
     }
     public void Quit()
     {
         Application.Quit();
+    }
+
+    public void ResetGameSession()
+    {
+        if (FindObjectOfType<GameSession>() !=null)
+        {
+            var gameSession = FindObjectOfType<GameSession>().gameObject;
+            Destroy(gameSession);
+        }
+
+        else
+        {
+            return;
+        }
+        
     }
 }
